b62cc3eb1b022d5c5c5fecbb06437ba3fd80252b
Vazgen <d.2100.v@gmail.com>
2023-10-11 13:42:20 +0400

    push

--- 11.10.2023/src/pages/DetailedPage/DetailedPage.jsx
+++ 11.10.2023/src/pages/DetailedPage/DetailedPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 const DetailedPage = () => {
-    // с помощю хука useParams беру динамичный id продукта для дальнейшего запроса для получения онформации о нем
+    // с помощю хука useParams беру динамичный id из ссыли(url) продукта для дальнейшего запроса для получения онформации о нем
     const { vazgenId } = useParams();
     const [product, setProduct] = useState({})
 
@@ -14,7 +14,6 @@ const DetailedPage = () => {
         })
     }
 
-    console.log({ product });
     useEffect(() => {
         getCurrrentProduct()
     }, [])
