01c51efc95be1162c601f4cbf07a54370e0fc8ef
Vazgen <d.2100.v@gmail.com>
2023-10-16 13:26:51 +0400

    push

--- 16.10.2023/src/App.js
+++ 16.10.2023/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { incrmenet } from './featrues/actions/action';
 
 const App = () => {
     // с помощю метода useSelector получаем глобальное общее хранилище данных
     // const result = useSelector(state=>state)
     // const counter = useSelector((vazgenState) => vazgenState.counter);
     const { counter } = useSelector((vazgenState) => vazgenState);
-   
+
+  // Создаем переменную dispatch котора ровна вызовы хука useDispatch
+  // Данная переменная явялется функции для передачии acio creator в редуюсер для изменения store
+    const dispatch = useDispatch();
+
+  const addToOne = () =>{
+    dispatch(incrmenet())
+  }
+
   return (
-    <div>{counter}</div>
+    <div>{counter}
+    <button onClick={addToOne}>+</button>
+    </div>
   )
 }
 
